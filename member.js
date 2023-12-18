c97abfbca0b86045518314a49a7071e055b4313b
Abhishek Tiwari <69783564+AbhishekT-2002@users.noreply.github.com>
2023-12-18 06:52:58 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillsMember() {
+    var members = [];
+    var member = {};
+    member.name = "John";
+    member.skill = "JavaScript";
+    members.push(member);
+    member = {};
+    member.name = "Bill";
+    member.skill = "C#";
+    members.push(member);
+    member = {};
+    member.name = "Mary";
+    member.skill = "Java";
+    members.push(member);
+    return members;
+}
